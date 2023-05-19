edd165788027f54be0f33c415d3aa3de12b9e36e
George <e.george.souza@mpt.mp.br>
2023-05-19 08:53:47 -0300

    commit dia 19/05/2023 as 8:53

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -26,8 +26,9 @@
 
                 <div class="input-container" id="opcionais-container">
                     <label id="opcionais-title" for="opcionais">Selecione os opcionais: </label>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" v-model="opcionais">
+                    <div class="checkbox-container" v-for="opcional in opcionaisData" :key="opcional.id">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                            <span>{{ opcional.tipo }}</span>
                     </div>
                 </div>
 
